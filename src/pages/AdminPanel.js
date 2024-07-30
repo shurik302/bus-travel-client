390509a830a229eed33fa94ca6833208e7fe3e62
shurik302 <balabuha1988@gmail.com>
2024-07-30 02:21:01 +0200

    fix

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
-import "../components/Sidebar.css";
+import '../components/Sidebar.css';
 import MainContent from '../components/MainContent';
 import axios from 'axios';
-import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
 
 function AdminPanel() {
-  const { t } = useTranslation();
   const [section, setSection] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUserRole = async () => {
       try {
         const accessToken = localStorage.getItem('accessToken');
+        console.log('Access token from localStorage:', accessToken);
         const response = await axios.get('/api/role', {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
         });
-        console.log('Current user role:', response.data.role); // Лог для перевірки ролі
+        console.log('Current user role:', response.data.role);
         if (response.data.role === 'admin') {
           setIsAdmin(true);
         }
       } catch (error) {
         console.error('Error fetching user role:', error);
+        setError('Failed to fetch user role.');
       } finally {
         setLoading(false);
       }
@@ -43,15 +43,16 @@ function AdminPanel() {
     return <div>Loading...</div>;
   }
 
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   if (!isAdmin) {
     return <div>Access denied. Admins only.</div>;
   }
 
   return (
     <div className='AdminPanel'>
-      <Helmet>
-        <title>{t('titles.admin-panel')}</title> {/* Установите заголовок страницы */}
-      </Helmet>
       <MainContent section={section} />
       <Sidebar onSelectSection={onSelectSection} />
     </div>
