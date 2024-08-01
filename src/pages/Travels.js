6bb36c6e12a6e02f480ca71743566b22452dcfe7
shurik302 <balabuha1988@gmail.com>
2024-08-01 02:52:41 +0200

    fix

--- src/pages/Travels.js
+++ src/pages/Travels.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 import '../stylesheets/Travels.css';
